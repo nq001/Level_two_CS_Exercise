f831109817a28d768a66ed3aa0e80e7e180d5570
Naif Alqubalee <129228748+nq001@users.noreply.github.com>
2023-11-04 08:33:52 +0300

    Add files via upload

---
+++ Assignment OOP Lab 1/Assignment OOP Lab 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace swap_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            int First_number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second number: ");
+            int second_number = Convert.ToInt32(Console.ReadLine());
+            int number = 0;  
+
+            number = First_number;
+            First_number = second_number;
+            second_number= number;
+
+            Console.WriteLine("the first number: {0} \nthe second number: {1}", First_number , second_number);
+            Console.ReadKey();
+
+        }
+    }
+}
