6e4643b5e192153db68e6dd54f0fa5e461b44cfc
Naif Alqubalee <129228748+nq001@users.noreply.github.com>
2023-11-04 08:27:56 +0300

    Add files via upload

---
+++ Power of number/OOP NEW/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power_of_number
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Enter basic number: ");
+            int basic = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Enter power number: ");
+            int power = Convert.ToInt32(Console.ReadLine());
+            int expo = power_num(basic, power);
+            Console.WriteLine($"Result is {expo}");
+            Console.ReadLine();
+
+        }
+        static int power_num(int num_basic, int num_power)
+        {
+            int result = 1;
+            for (int i = 0; i < num_power; i++)
+            {
+                result *= num_basic;
+            }
+            return result;
+        }
+    }
+}
---
+++ prime number/prime number/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Enter a number: ");
+            int pnum = int.Parse(Console.ReadLine());
+            if (primenum(pnum))
+            {
+                Console.WriteLine($"{pnum} this is a prime number! ");
+            }
+            else
+            {
+                Console.WriteLine($"{pnum} this is not a prime number! ");
+            }
+            Console.ReadLine();
+
+        }
+        public static bool primenum(int num)
+        {
+           if (num <= 1)
+           {
+                return false;
+           }
+
+           for (int i = 2; i <= num/2; i++)
+           {
+                
+                if (num % i == 0)
+                {
+                    return false;
+                }
+           }
+           return true;
+        }
+        
+    }
+
+}
